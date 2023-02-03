38b04bc52152128b3b7d98b0c516672b49927021
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-03 23:40:02 +0100

    created component for single saint and included

---
+++ resources/views/components/saint.blade.php
@@ -0,0 +1,8 @@
+    <a href="/saint/{{$saint["id"]}}">
+        <div class="myCard d-flex flex-column"> 
+            <span>Saint {{ $saint["nome"] }} </span>
+            <span>{{ $saint["luogoDiNascita"] }} </span>
+            <span>{{ $saint["dataDiNascita"] }} </span>
+            <span>{{ $saint["numeroDiMiracoli"] }} </span>
+        </div>
+    </a>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,15 +2,9 @@
 
 @section('contents')
 <div class="container d-flex flex-wrap justify-content-center">
+
     @foreach ($saints as $saint)
-    <a href="/saint/{{$saint["id"]}}">
-        <div class="myCard d-flex flex-column"> 
-            <span>Saint {{ $saint["nome"] }} </span>
-            <span>{{ $saint["luogoDiNascita"] }} </span>
-            <span>{{ $saint["dataDiNascita"] }} </span>
-            <span>{{ $saint["numeroDiMiracoli"] }} </span>
-        </div>
-    </a>
+        @include('components.saint')
     @endforeach
 </div>
 
