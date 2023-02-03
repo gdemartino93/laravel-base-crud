a767c86d7c2914e994f7952d5931c16100323986
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-04 00:47:19 +0100

    intalled fontawesome and used icon for delete

--- resources/views/components/saint.blade.php
+++ resources/views/components/saint.blade.php
@@ -4,6 +4,9 @@
         <span>{{ $saint["luogoDiNascita"] }} </span>
         <span>{{ $saint["dataDiNascita"] }} </span>
         <span>{{ $saint["numeroDiMiracoli"] }} </span>
-        <a href="{{route('saint.delete',['id' => $saint -> id])}}"> ELIMINA </a>
+        <a href="{{route('saint.delete',['id' => $saint -> id])}}"> 
+            <i class="fa-solid fa-square-minus"></i>
+        </a>
+
     </div>
 </a>
